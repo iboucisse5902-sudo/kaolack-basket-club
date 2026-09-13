5a3e86ed390fee26f64a7db1ebf492e05ea5d5e7
Khalil <iboucisse5902@gmail.com>
2026-09-13 23:35:45 +0000

    Correction indentation app.py

--- app.py
+++ app.py
@@ -54,6 +54,6 @@ def ajouter_match():
         return redirect("/")
 
     if __name__ == "__main__":
-     import os
-     port = int(os.environ.get("PORT", 5000))
-     app.run(host="0.0.0.0", port=port)
+        import os
+        port = int(os.environ.get("PORT", 5000))
+        app.run(host="0.0.0.0", port=port)
